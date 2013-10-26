2ae2541afa317c3fafd24f86527cbebe64ccd48f
thekillerchrist <thekillerchrist@gmail.com>
2013-10-26 19:58:54 -0400

    Removing itemCost from calculateSubtotal and only returning subtotal

--- calculate.js
+++ calculate.js
@@ -3,10 +3,10 @@
 exports.calculateSubtotal = function(items){
 	var items_cost = 0;
 	for (var i = 0; i < items.length; i++) {
-		items_cost += items[i].qty * items[i].price;
+		items_cost += items[i].quantity * items[i].price;
 	}
-	var subtotal = items_cost + (items_cost * 0.18) + (items_cost * 0.08875); 
-	return {itemCost: items_cost, subtotal: subtotal};
+	var subtotal = items_cost + (items_cost * 0.18) + (items_cost * 0.08875);
+	return subtotal;
 };
 
 exports.calculateGroupTotal = function(group_total, user_data){
