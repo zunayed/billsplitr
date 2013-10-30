ed4fbf615b6abeb935e4377c127ceb0eb5a5a952
Zunayed Ali <zunayed@gmail.com>
2013-10-30 13:16:33 -0400

    moved functions to collectData

--- public/frontend.js
+++ public/frontend.js
@@ -4,42 +4,41 @@ var username = ''
 $(function() {
    $('#itemControl').hide();
    // $('#groupTotal').hide();
-   $("#usernameSet").click(function() {setUsername()});
-   $("#addItem").click(function() {addItem()});
+   $("#usernameSet").click( function() { collectData() } );
+   $("#addItem").click( function() { collectData() } );
 
 });
 
-function addItem () {
-   var quantity = $('#quantity').val();
-   var price = $('#price').val();
-   var description = $('#description').val();
-   var items = [];
-
-   var msg =  quantity + " " + description + " each costing " + price;
-   $("#itemList").append("<li class='list-group-item' data-price='" + price + "' data-quantity='" + quantity + "' data-description='" + description + "'>" + msg + "</li>");
-
-   $("#itemList li").each(function (index, li){
-      var quantity = $(li).data('quantity');
-      var price = $(li).data('price');
-      var description = $(li).data('description');
-      items.push({
-         quantity: quantity,
-         price: price,
-         description: description
-      });
-   });
-   //returns user data 
-   socket.emit('addItem', {name: username, items: items})
-}
-
-function setUsername() {
+function collectData() { 
    username = $("#usernameInput").val()
-   if (username != "")
-   {
-      socket.emit('addNewUser', username);
+   if ( username != "" ) {
+      socket.emit('addInfo', {name: username, items: []})
       $('#usernameInput').hide();
       $('#usernameSet').hide();
       $('#itemControl').show();
+
+      var quantity = $('#quantity').val();
+      var price = $('#price').val();
+      var description = $('#description').val();
+      var items = [];
+
+      if (description != "") {
+         var msg =  quantity + " " + description + " each costing " + price;
+         $("#itemList").append("<li class='list-group-item' data-price='" + price + "' data-quantity='" + quantity + "' data-description='" + description + "'>" + msg + "</li>");
+
+         $("#itemList li").each(function (index, li){
+            var quantity = $(li).data('quantity');
+            var price = $(li).data('price');
+            var description = $(li).data('description');
+            items.push({
+               quantity: quantity,
+               price: price,
+               description: description
+            });
+         });
+         //returns user data 
+         socket.emit('addInfo', {name: username, items: items})
+      }
    }
 }
 
@@ -48,6 +47,7 @@ socket.on('peopleInRoom', function(people) {
    for(var i = 0; i < people.length; i++){
       var person = people[i];
       $("#userList").append("<li class='list-group-item'>" + person + " in the room</li>");
+      // $("#userList").append("<ul id='itemList'></ul>");
    };
 })
 
