148683d4f7318991a1b08cd66ea5e2a4c52fb8e5
Zunayed Ali <zunayed@gmail.com>
2013-10-31 15:14:44 -0400

    removed console.logs

--- calculate.js
+++ calculate.js
@@ -2,9 +2,6 @@
 // [{"qty":1,"price":5,"description":"beer"},{"qty":1,"price":5,"description":"burger"}]
 exports.calculateSubtotal = function(items){
 	var items_cost = 0;
-	  console.log('calculateSubtotal')
-      console.log(items)
-
 	for (var i = 0; i < items.length; i++) {
 		items_cost += items[i].quantity * items[i].price;
 	}
