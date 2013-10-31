0d942e31ff1bd6674c323189facfe563f363f900
Zunayed Ali <zunayed@gmail.com>
2013-10-30 21:02:40 -0400

    removed bootstrap

--- server.js
+++ server.js
@@ -14,43 +14,45 @@ app.set( 'view engine', 'jade' );
 app.set( "view options", { layout: false } );
 app.configure( function() {
 	app.use(express.static( __dirname + '/public' ));
+	app.use(express.static( __dirname + '/public/css' ));
+
 });
- 
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+    app.locals.pretty = true;
+});
+
 app.get('/', function( req, res ){
   res.render( 'home.jade' );
 });
 
-billsplitr = new models.App();
- 
+
+bs = new models.App();
+
 io.sockets.on('connection', function ( socket ) {
 
+	function emit( channel , data )  {
+		socket.emit( channel, data );
+		socket.broadcast.emit( channel, data );
+	}
+
 	//broadcast all people in room
-	socket.emit( 'peopleInRoom', billsplitr.people_list );
-	socket.broadcast.emit( 'peopleInRoom', billsplitr.people_list );
+	emit( 'peopleInRoom', bs.people_list );
 	//Group total
-	socket.emit( 'groupTotal', Math.round( billsplitr.group_total ) );
-	socket.broadcast.emit( 'groupTotal', Math.round( billsplitr.group_total ) );
- 
-	// set username
-	// socket.on('addNewUser', function ( name ) {
-	// 	billsplitr.addPerson( name );
-	// 	socket.emit( 'peopleInRoom', billsplitr.people_list );
-	// 	socket.broadcast.emit( 'peopleInRoom', billsplitr.people_list );
-	// });
+	emit( 'groupTotal', Math.round( bs.group_total ) );	
 
 	//add a new item
 	socket.on('addInfo', function ( data ) {
-		billsplitr.addPerson( data.name );
-		person_index = billsplitr.getPerson(data.name)
-		for (var i = data.items.length - 1; i >= 0; i--) {
-				billsplitr.people[person_index].addItem( data.items[i] );
-		};
-		socket.emit( 'peopleInRoom', billsplitr.people_list );
-		socket.broadcast.emit( 'peopleInRoom', billsplitr.people_list );
-		socket.emit( 'groupTotal', Math.round( billsplitr.group_total ) );
-		socket.broadcast.emit( 'groupTotal', Math.round( billsplitr.group_total ) );
+		bs.addPerson( data.name );
+		person_index = bs.getPerson(data.name)
+		if (data.items.length > 0) {
+			for (var i = data.items.length - 1; i >= 0; i--) {
+				bs.people[person_index].addItem( data.items[i] );
+			};
+		}
+		emit( 'peopleInRoom', bs.people_list );
+		emit( 'groupTotal', Math.round( bs.group_total ) );
 
 	});
-
-
 });
